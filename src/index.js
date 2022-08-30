7ecc35466aebaca0bdbee070461388c3af89601f
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-08-30 23:17:04 +0300

    3008

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box')
+const listEl = document.querySelector('.country-list')
+
+
+inputEl.addEventListener('input', debounce(onSearchboxInput, DEBOUNCE_DELAY))
+
+function onSearchboxInput() {
+    fetchCountries(inputEl.value)
+}
+
+
+
+function fetchCountries(name) {
+fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(resolve => resolve.json())
+    .then(countries => {
+        console.log(countries);
+        for (const country of countries) {
+            const marcup = `<li><img src="${country.flags.svg}" alt="flag" width = "80" height = "80" ></li>`
+            listEl.innerHTML = marcup
+        }
+    })
+.catch(error => console.log(error))
+}
+
+
