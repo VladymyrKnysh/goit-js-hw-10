afaa0f6fc9bd3b02d2180e9b9725de7b7b549156
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-08-31 16:48:11 +0300

    ggg

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+// import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box')
@@ -18,13 +19,24 @@ function fetchCountries(name) {
 fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
     .then(resolve => resolve.json())
     .then(countries => {
-        console.log(countries);
-        for (const country of countries) {
-            const marcup = `<li><img src="${country.flags.svg}" alt="flag" width = "80" height = "80" ></li>`
-            listEl.innerHTML = marcup
+        if (countries.length > 5) {
+          return  alert('Слишком много совпадений')
         }
+        const marcup = countries
+            .map(({ name, capital, population, flags, languages }) =>
+             `<li class="country-item">
+            <img src="${flags.svg}" alt="flag" width = "20" height = "15">
+            <p class="country-title">${name.common}</p>
+            </li>`)
+            .join('')
+         listEl.innerHTML = marcup
+     console.log(countries.length);
     })
 .catch(error => console.log(error))
 }
 
 
+//    for (const country of countries) {
+//             const marcup = `<li><img src="${country.flags.svg}" alt="flag" width = "80" height = "80" ></li>`
+//            
+//         }
