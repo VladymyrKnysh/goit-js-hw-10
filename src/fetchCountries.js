5094e13f638cfc930325b6d5882793be36b46649
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-09-01 21:57:42 +0300

    hw10end

---
+++ src/fetchCountries.js
@@ -0,0 +1,3 @@
+
+  export const fetchCountries = name => fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+  
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import {fetchCountries} from './fetchCountries'
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
@@ -7,42 +8,53 @@ const inputEl = document.querySelector('#search-box')
 const listEl = document.querySelector('.country-list')
 const infoBox = document.querySelector('.country-info')
 
-
-
-
 inputEl.addEventListener('input', debounce(onSearchboxInput, DEBOUNCE_DELAY))
 
 function onSearchboxInput() {
-  
-    fetchCountries(inputEl.value.trim())
-    
-}
-
-
-
-function fetchCountries(name) {
-fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-    .then(resolve => resolve.json())
+   fetchCountries(inputEl.value.trim())
+   .then(resolve => {
+        if (!resolve.ok) {
+        throw new Error(resolve.status);
+      }
+        return resolve.json()   
+    } )
     .then(countries => {
         if (countries.length > 10) {
             listEl.innerHTML = ''
-            return Notiflix.Notify.warning("Too many matches found. Please enter a more specific name.");
+             Notiflix.Notify.warning("Too many matches found. Please enter a more specific name.");
             
         } else if (countries.length <= 10 & countries.length > 1) {
-                const marcup = countries
+          renderMarcupList(countries)
+     
+        } else {
+            listEl.innerHTML = ''
+           renderMarcupBox(countries)
+        }
+    })
+    .catch(error => {
+        listEl.innerHTML = ''
+        infoBox.innerHTML = ''
+        console.log(error);
+        Notiflix.Notify.failure("Oops, there is no country with that name")
+        // Notiflix.Report.failure('извините, вы ищите какую-то хрень')
+
+})
+}
+
+function renderMarcupList(countries) {
+      const marcup = countries
             .map(({ name,flags,}) =>
              `<li class="country-item"><img src="${flags.svg}" alt="flag" width = "20" height = "15"><p class="country-title">${name.common}</p></li>`)
             .join('')
             listEl.innerHTML = marcup
             infoBox.innerHTML = ''
-     
-        } else {
-            listEl.innerHTML = ''
-            const marcupInfo = countries
+}
+function renderMarcupBox(countries) {
+ const marcup = countries
                 .map(({ name, capital, population, flags, languages }) =>
                  `<div class="country-info-box">
                     <img src="${flags.svg}" alt="flag" width="20" height="15">
-                     <h2 class = "country-info-title">${name.common}</h2>
+                     <h2 class = "country-info-title">${name.official}</h2>
                     </div>
                     <ul class="country-info-list">
                     <li class="country-info-item"><span class="country-info-span">Capital:</span> ${capital}</li>
@@ -50,19 +62,5 @@ fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,populatio
                     <li class="country-info-item"><span class="country-info-span">Languages:</span> ${Object.values(languages)}</li>
                     </ul>`)
                 .join('')
-            infoBox.innerHTML = marcupInfo
-            
-            
-        }
-
-    
-    })
-    .catch(error => {
-        listEl.innerHTML = ''
-        infoBox.innerHTML = ''
-        console.log(error);
-        Notiflix.Notify.failure("Oops, there is no country with that name")
-})
+            infoBox.innerHTML = marcup
 }
-
-
